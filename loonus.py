b36aba5444b88ef09e0c0dac8c6a87ff6747a6ef
Jaideep Singh <jaideepsingh@Jaideeps-MBP.lan>
2016-03-14 22:18:58 -0500

    Added basic function that outputs random 6 digit string

--- loonus.py
+++ loonus.py
@@ -1,5 +1,5 @@
 #import stuff
-import sqlite3
+import sqlite3, string, random
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 #configuration
@@ -35,16 +35,17 @@ def teardown_request(exception):
 
 @app.route('/')
 def show_entries():
-    cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    cur = g.db.execute('select original, shorturl from entries order by id desc')
+    entries = [dict(original=row[0], shorturl=row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
+
+def shorturlcalc(originalurl):
+	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    g.db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
+    su = shorturlcalc(request.form['original'])
+    g.db.execute('insert into entries (original, shorturl) values (?, ?)',[request.form['original'], su])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
@@ -52,15 +53,9 @@ def add_entry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
+    session['logged_in'] = True
+    flash('You were logged in')
+    return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
